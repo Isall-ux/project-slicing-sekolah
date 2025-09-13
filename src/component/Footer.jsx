6552ce5eb7aca63449b828c0c43e8c7365403ca3
Isall <tetroverse4@gmail.com>
2025-09-13 15:15:01 +0700

    commit

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -14,13 +14,13 @@ const Footer = () => {
   }
 ]
   return (
-    <footer className="p-6 text-white bg-blue-800 rounded w-full flex flex-col sm:flex-row items-center justify-between">
+    <footer className="p-6 text-white bg-blue-800 w-full flex items-center justify-between">
       <div className="flex-1 flex justify-center order-1 sm:order-none">
         <p className="text-center">All rights reserve to SMK YADIKA SOREANG 2025 Ltd.</p>
       </div>
-      <div className="flex flex-row space-x-3 order-2 mt-2">
+      <div className="flex flex-row space-x-3 order-2">
         {mediaSosial.map((item) => (
-          <a href={item.link} key={item.link} target="_blank" rel="noopener noreferrer">
+          <a href={item.link} key={item.link} target="_blank" rel="noopener">
             <img src={item.img} alt={item.alt} className="w-6 h-6" />
           </a>
         ))}
--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -8,8 +8,8 @@ import ButtonRound from './ButtonRound.jsx'
 const Nav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className="flex items-center justify-between px-4 sm:px-10 py-5 border-b-2 border-gray-200 w-full relative z-20">
-      <nav className="flex items-center flex-row sticky top-0 w-full">
+    <div className="flex items-center justify-between px-4 sm:px-10 py-5 border-b-2 border-gray-200 w-full sticky top-0 z-20 bg-white">
+      <nav className="flex items-center flex-row w-full">
         <BrandLabel />
         {/* Hamburger icon for mobile */}
         <button
